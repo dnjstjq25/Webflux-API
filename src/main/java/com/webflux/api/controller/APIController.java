9ee88852fb0612c63dac97b3d5fcf642ebf0e93f
hyosung.ui <hyosungjang.ui@gmail.com>
2020-12-07 15:59:54 +0900

    [20.12.07]
    - Webflux API(main)
      - 전체적인 구조 다시 변경
      - 1 Repository에 1 DTO를 사용해야하는데
        그러면 조인할때마다 새롭게 만들어줘야 되는건가

--- src/main/java/com/webflux/api/controller/APIController.java
+++ src/main/java/com/webflux/api/controller/APIController.java
@@ -1,44 +1,61 @@
 package com.webflux.api.controller;
 
+import com.webflux.api.model.Car;
 import com.webflux.api.model.Member;
 import com.webflux.api.model.Version;
 import com.webflux.api.service.APIService;
-import lombok.extern.log4j.Log4j;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class APIController {
-
+    //사용할 서비스 선언
     private final APIService apiService;
-
-
+    //서비스 초기화
     public APIController(APIService apiService) {
         this.apiService = apiService;
     }
 
+    //버전체크
     @GetMapping("/version")
     public Mono<Version> version() throws Exception {
 
         return apiService.versionSelect();
     }
 
+    //로그인
     @PostMapping("/login")
     public Mono<Member> login(@RequestBody Member member) throws Exception {
 
         return apiService.login(member);
     }
 
+    //회원가입 또는 회원정보 수정
     @PostMapping("/memberRegister")
     public Mono<Member> register(@RequestBody Member member) throws Exception {
 
         return apiService.register(member);
     }
 
+    //회원탈퇴
     @PostMapping("/memberDelete")
     public Mono<Void> delete(@RequestBody Member member) throws Exception {
 
         return apiService.delete(member);
     }
+
+    //전체차량조회
+    @GetMapping(value = "/allCar")
+    public Flux<Car> allCar() {
+
+        return apiService.allCar();
+    }
+    //특정차량조회
+    @PostMapping(value = "/oneCar")
+    public Mono<Car> oneCar(@RequestBody Car car){
+
+        return apiService.oneCar(car);
+    }
+
 }
--- src/main/java/com/webflux/api/service/APIService.java
+++ src/main/java/com/webflux/api/service/APIService.java
@@ -1,7 +1,9 @@
 package com.webflux.api.service;
 
+import com.webflux.api.mapper.CarRepository;
 import com.webflux.api.mapper.MemberRepository;
 import com.webflux.api.mapper.VersionRepository;
+import com.webflux.api.model.Car;
 import com.webflux.api.model.Member;
 import com.webflux.api.model.Version;
 import lombok.extern.slf4j.Slf4j;
@@ -9,36 +11,57 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class APIService {
 
     private final VersionRepository versionRepository;
     private final MemberRepository memberRepository;
+    private final CarRepository carRepository;
 
-    public APIService(VersionRepository versionRepository, MemberRepository memberRepository) {
+    public APIService(VersionRepository versionRepository, MemberRepository memberRepository, CarRepository carRepository) {
         this.versionRepository = versionRepository;
         this.memberRepository = memberRepository;
+        this.carRepository = carRepository;
     }
 
+    //버전체크
     public Mono<Version> versionSelect() {
 
         return versionRepository.versionSelect();
     }
-
+    //로그인
     public Mono<Member> login(Member member) {
 
         return memberRepository.login(member);
     }
-
+    //회원가입 또는 회원정보 수정
     public Mono<Member> register(Member member) {
 
         return memberRepository.save(member); //insert, update를 처리(기본키가 있을 때)
     }
-
+    //회원탈퇴
     public Mono<Void> delete(Member member) {
 
         return memberRepository.delete(member);
+    }
+    //전체차량조회
+    public Flux<Car> allCar() {
+        Flux<Car> allCar = carRepository.findAll();
+        List<Car> blocked = allCar.collectList().block();
+        /*log.info(blocked.toString());*/
+        blocked.forEach(item -> log.info("user : {}", item));
+
+        return allCar;
+    }
+    //특정차량조회
+    public Mono<Car> oneCar(Car car) {
+
+        String car_number = car.getCar_number();
+
+        return carRepository.findOneCar(car_number);
     }
 
 }
